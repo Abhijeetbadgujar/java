66e54619e3bbe2f45d154c53848a6c1d5454a8c7
Abhijeetbadagujar <abhijeet.xworkz@gmail.com>
2021-10-20 11:09:14 +0530

    Array program

---
+++ ArrayStater.java
@@ -0,0 +1,321 @@
+package arrayStater.xworkz.Interger;
+
+public class ArrayStater {
+
+	public static void main(String[] args) {
+		//phone number
+		long abhi=9089892744L;
+		long pooja=9089892745L;
+		long vandana=9089892746L;
+		long kiran=9089892747L;
+		long gomu=9089892748L;
+		long sanju=9089892749L;
+		/*long ambika;//=9089892741L;
+		long akshita;//=9089892742L;
+		long sangita;//=9089892743L;
+		long appa;//=9089892750L;
+		long arjun;//=9089892751L;*/
+		
+		long[] phoneNO={abhi,pooja,vandana,kiran,gomu,sanju,
+				9089892741L,9089892742L,9089892743L,9089892750L,9089892751L};
+
+		System.out.println("abhi:"+abhi);
+		System.out.println("pooja:"+pooja);
+		System.out.println("vandana:"+vandana);
+		System.out.println("gomu:"+gomu);
+		System.out.println("sanju:"+sanju);
+		System.out.println("kiran:"+kiran);
+		//System.out.println("ambika:"+ambika);
+		//phoneNO[7]=7276759230L;
+		long index=phoneNO[6];
+		System.out.println("change the  number:"+index);
+		phoneNO[7]=7276759231L;
+		long index1=phoneNO[7];
+		System.out.println("change ambika number:"+index1);
+		phoneNO[8]=7276759232L;
+		long index2=phoneNO[8];
+		System.out.println("change sangita number:"+index2);
+		phoneNO[9]=7276759233L;
+		long index3=phoneNO[9];
+		System.out.println("change appa number:"+index3);
+		phoneNO[10]=7276759234L;
+		long index5=phoneNO[10];
+		System.out.println("change arjun number:"+index5);
+	
+
+	//public static void main(String[] args) {
+		//aadhar card
+		long abhij=654467126287L;
+		long pooja1=654467126288L;
+		long vandana1=654467126289L;
+		long kiran1=654467126290L;
+		long gomu1=654467126291L;
+		long sanju1=654467126292L;
+		/*double ambika;//=9089892741L;
+		double akshita;//=9089892742dL;
+		double sangita;//=9089892743dL;
+		double appa;//=9089892750L;
+		double arjun;//=9089892751L;*/
+		
+		long[] aadharNo={abhij,pooja1,vandana1,kiran1,gomu1,sanju1,
+				654467126293L,654467126294L,654467126295L,654467126296L,654467126297L};
+	 
+		System.out.println("abhij:"+abhij);
+		System.out.println("pooja:"+pooja1);
+		System.out.println("vandana:"+vandana1);
+		System.out.println("gomu:"+gomu1);
+		System.out.println("sanju:"+sanju1);
+		System.out.println("kiran:"+kiran1);
+		long ref=aadharNo[6];
+		System.out.println("change the  number:"+ref);
+		aadharNo[7]=754467126293L;
+		long ref1=aadharNo[7];
+		System.out.println("change ambika aadhar number:"+ref1);
+		aadharNo[8]=754467126293L;
+		long ref2=aadharNo[8];
+		System.out.println("change appa aadhar number:"+ref2);
+		aadharNo[9]=754467126293L;
+		long ref3=aadharNo[9];
+		System.out.println("change arjun aadhar number:"+ref3);
+		aadharNo[10]=754467126293L;
+		long ref4=aadharNo[10];
+		System.out.println("change ambika aadhar number:"+ref4);
+		
+		//bank acc
+		long abhij1=754467126287L;
+		long pooja2=754467126288L;
+		long vandana2=754467126289L;
+		long kiran2=754467126290L;
+		long gomu2=754467126291L;
+		long sanju2=754467126292L;
+		
+		long[] bankAcc={abhij1,pooja2,vandana2,kiran2,gomu2,sanju2,
+				754467126293L,754467126294L,754467126295L,754467126296L,754467126297L};
+	 
+		System.out.println("abhij1:"+abhij1);
+		System.out.println("pooja1:"+pooja2);
+		System.out.println("vandana1:"+vandana2);
+		System.out.println("gomu1:"+gomu2);
+		System.out.println("sanju1:"+sanju2);
+		System.out.println("kiran1:"+kiran2);
+		long l=bankAcc[6];
+		System.out.println("account number:"+l);
+		bankAcc[7]=7276759231L;
+		long l1=bankAcc[7];
+		System.out.println("change ambika account number:"+l1);
+		bankAcc[8]=7276759232L;
+		long l2=bankAcc[8];
+		System.out.println("change sangita account number:"+l2);
+		bankAcc[9]=7276759233L;
+		long l3=bankAcc[9];
+		System.out.println("change appa account number:"+l3);
+		aadharNo[10]=7276759234L;
+		long l4=aadharNo[10];
+		System.out.println("change arjun account number:"+l4);
+		
+		//pin code
+		int abhij2=431001;
+		int pooja3=431002;
+		int vandana3=431003;
+		int kiran3=431004;
+		int gomu3=431005;
+		int sanju3=431006;
+		
+		int[] pinCode={abhij2,pooja3,vandana3,kiran3,gomu3,sanju3,
+				431007,431008,431009,431010,431011};
+	 
+		System.out.println("abhij2:"+abhij2);
+		System.out.println("pooja2:"+pooja3);
+		System.out.println("vandana2:"+vandana3);
+		System.out.println("gomu2:"+gomu3);
+		System.out.println("sanju2:"+sanju3);
+		System.out.println("kiran2:"+kiran3);
+		int i=pinCode[6];
+		System.out.println("account number:"+i);
+		pinCode[7]=531007;
+		int i1=pinCode[7];
+		System.out.println("change ambika account number:"+i1);
+		pinCode[8]=531007;
+		int i2=pinCode[8];
+		System.out.println("change sangita account number:"+i2);
+		pinCode[9]=531007;
+		int i3=pinCode[9];
+		System.out.println("change appa account number:"+i3);
+		pinCode[10]=531007;
+		int i4=pinCode[10];
+		System.out.println("change arjun account number:"+i4);
+		
+		//Order id
+		int abhij3=1002;
+		int pooja4=1003;
+		int vandana4=1004;
+		int kiran4=1005;
+		int gomu4=1006;
+		int sanju4=1007;
+		
+		int[] orderId={abhij3,pooja4,vandana4,kiran4,gomu4,sanju4,
+				1008,1009,1012,1022,1032};
+	 
+		System.out.println("abhij3:"+abhij3);
+		System.out.println("pooja3:"+pooja4);
+		System.out.println("vandana3:"+vandana4);
+		System.out.println("gomu3:"+gomu4);
+		System.out.println("sanju3:"+sanju4);
+		System.out.println("kiran3:"+kiran4);
+		int id=orderId[6];
+		System.out.println("account number:"+id);
+		orderId[7]=1027;
+		int id1=orderId[7];
+		System.out.println("change ambika orderId:"+id1);
+		orderId[8]=1026;
+		int id2=orderId[8];
+		System.out.println("change sangita orderId:"+id2);
+		orderId[9]=1025;
+		int id3=orderId[9];
+		System.out.println("change appa orderId:"+id3);
+		orderId[10]=1024;
+		int id4=orderId[10];
+		System.out.println("change arjun orderId:"+id4);
+		
+		//Mark card number
+		int abhij4=60;
+		int pooja5=61;
+		int vandana5=62;
+		int kiran5=63;
+		int gomu5=64;
+		int sanju5=65;
+		
+		int[] markCard={abhij2,pooja3,vandana3,kiran3,gomu3,sanju3,
+				70,66,67,68,69};
+	 
+		System.out.println("abhij4:"+abhij4);
+		System.out.println("pooja4:"+pooja5);
+		System.out.println("vandana4:"+vandana5);
+		System.out.println("gomu4:"+gomu5);
+		System.out.println("sanju4:"+sanju5);
+		System.out.println("kiran4:"+kiran5);
+		int mark=markCard[6];
+		System.out.println("account mark:"+mark);
+		markCard[7]=90;
+		int mark1=markCard[7];
+		System.out.println("change ambika mark:"+mark1);
+		markCard[8]=91;
+		int mark2=markCard[8];
+		System.out.println("change sangita mark:"+mark2);
+		markCard[9]=92;
+		int mark3=markCard[9];
+		System.out.println("change appa mark:"+mark3);
+		markCard[10]=93;
+		int mark4=markCard[10];
+		System.out.println("change arjun mark:"+mark4);
+		
+		//Roll NO
+		int abhij5=10;
+		int pooja6=01;
+		int vandana6=02;
+		int kiran6=03;
+		int gomu6=04;
+		int sanju6=05;
+		
+		int[] rollNO={abhij5,pooja6,vandana6,kiran6,gomu6,sanju6,
+				07,43,035,11,12};
+	 
+		System.out.println("abhij5:"+abhij5);
+		System.out.println("pooja5:"+pooja6);
+		System.out.println("vandana5:"+vandana6);
+		System.out.println("gomu5:"+gomu6);
+		System.out.println("sanju5:"+sanju6);
+		System.out.println("kiran5:"+kiran6);
+		int rollno=rollNO[6];
+		System.out.println("account mark:"+rollno);
+		rollNO[7]=90;
+		int rollNO1=rollNO[7];
+		System.out.println("change ambika mark:"+rollNO1);
+		rollNO[8]=91;
+		int rollN2=rollNO[8];
+		System.out.println("change appa mark:"+rollN2);
+		rollNO[9]=92;
+		int rollN3=rollNO[9];
+		System.out.println("change arjun mark:"+rollN3);
+		rollNO[10]=93;
+		int rollN4=rollNO[10];
+		System.out.println("change sangita mark:"+rollN4);
+		
+		//OTP
+		long otp=1234l;
+		long otp1=1235L;
+		long otp2=1236L;
+		long otp3=1237l;
+		long otp4=1238l;
+		long otp5=1239l;
+		long otp6=1230l;
+		long otp7=1231l;
+		long otp8=1232l;
+		long otp9=1233l;
+		long oTp[]= {otp,otp1,otp2,otp3,otp4,otp5,otp6,otp7,otp8,otp9};
+		for(int x=0;x<oTp .length;x++) {
+			System.out.println("otp:"+oTp[x]);
+		}
+		//Application
+		int app=01;
+		int app1=02;
+		int app2=03;
+		int app3=04;
+		int app4=05;
+		int app5=06;
+		int app6=07;
+		int app7=13;
+		int app8=12;
+		int app9=10;
+		int App[]= {app,app1,app2,app3,app4,app5,app6,app7,app8,app9};
+		for(int x=0;x<App.length;x++) {
+			System.out.println("Applicatin no:"+App[x]);
+		}
+		//IP Address
+		long ip=123423l;
+		long ip1=123545L;
+		long ip2=123622L;
+		long ip3=123744l;
+		long ip4=123855l;
+		long ip5=123966l;
+		long ip6=123077l;
+		long ip7=123188l;
+		long ip8=123829l;
+		long ip9=123343l;
+		long Ip[]= {ip,ip1,ip2,ip3,ip4,ip5,ip6,ip7,ip8,ip9};
+		for(int x=0;x<Ip.length;x++) {
+			System.out.println("otp:"+Ip[x]);
+		}
+
+		//Door no
+		int door=111;
+		int door1=222;
+		int door2=333;
+		int door3=444;
+		int door4=555;
+		int door5=666;
+		int door6=777;
+		int door7=837;
+		int door8=928;
+		int door9=1309;
+		int DoorNO[]= {door,door1,door2,door3,door4,door5,door6,door7,door8,door9};
+		for(int x=0;x<DoorNO.length;x++) {
+			System.out.println("Applicatin no:"+DoorNO[x]);
+		}
+		//imei
+		long im=1023423l;
+		long im1=923545L;
+		long im2=823622L;
+		long im3=723744l;
+		long im4=623855l;
+		long im5=523966l;
+		long im6=423077l;
+		long im7=323188l;
+		long im8=223829l;
+		long im9=1223343l;
+		long Im[]= {im,im1,im2,im3,im4,im5,im6,im7,im8,im9};
+		for(int x=0;x<Im.length;x++) {
+			System.out.println("otp:"+Im[x]);
+		}
+	}
+}
---
+++ StringArray.java
@@ -0,0 +1,160 @@
+package arrayStater.xworkz.Interger;
+
+public class StringArray {
+
+	public static void main(String[] args) {
+		//pancard
+		String pan="EEDPB2335J";
+		String pan1="EEDPB2335k";
+		String pan2="EEDPB2335l";
+		String pan3="EEDPB2335M";
+		String pan4="EEDPB2335N";
+		String pan5="EEDPB2335B";
+		String pan6="EEDPB2335V";
+		String pan7="EEDPB2335X";
+		String pan8="EEDPB2335S";
+		String pan9="EEDPB2335Z";
+		String panCard[]= {pan,pan1,pan2,pan3,pan4,pan5,pan6,pan7,pan8,pan9};
+		for (String p:panCard) {
+			System.out.println("PanCard no:"+p);
+		}
+		// Vehical no
+		String veh="MH 20 cd 9053";
+		String veh1="MH 20 cd 9054";
+		String veh2="MH 20 cd 9055";
+		String veh3="MH 20 cd 9056";
+		String veh4="MH 20 cd 9057";
+		String veh5="MH 20 cd 9058";
+		String veh6="MH 20 cd 9059";
+		String veh7="MH 20 cd 9051";
+		String veh8="MH 20 cd 9052";
+		String veh9="MH 20 cd 9050";
+		String Vehical[]= {veh,veh1,veh2,veh3,veh4,veh5,veh6,veh7,veh8,veh9,};
+		for (String p:Vehical) {
+			System.out.println("vehical no:"+p);
+		}
+		// Passport no
+		String pa="EEDPB2335";
+		String pa1="EEDPB233";
+		String pa2="EEDPB233";
+		String pa3="EEDPB233";
+		String pa4="EEDPB23N";
+		String pa5="EEDPB35B";
+		String pa6="EED2335V";
+		String pa7="EPB2335X";
+		String pa8="EPB2335S";
+		String pa9="DPB2335Z";
+		String passPort[]= {pa,pa1,pa2,pa3,pa4,pa5,pa6,pa7,pa8,pa9};
+		for (String p:passPort) {
+			System.out.println("passPort no:"+p);
+		}
+		//GST
+		String gs="EEDPB2335qqqqq";
+		String gs1="EEDPB233wwwww";
+		String gs2="EEDPB233eeeee";
+		String gs3="EEDPB233rrrrr";
+		String gs4="EEDPB23Nwwddd";
+		String gs5="EEDPB35Bfffff";
+		String gs6="EED233ddddd5V";
+		String gs7="EPB2335aaaaaX";
+		String gs8="EPB2335aaaaaS";
+		String gs9="DPB2335cccccZ";
+		String gSt[]= {gs,gs1,gs2,gs3,gs4,gs5,gs6,gs7,gs8,gs9};
+		for (String p:gSt) {
+			System.out.println("GST no:"+p);
+		}	
+			//Applicatin version
+			
+			String ap="real1";
+			String ap1="real17";
+			String ap2="real12";
+			String ap3="real11";
+			String ap4="real14";
+			String ap5="real12";
+			String ap6="real13";
+			String ap7="real15";
+			String ap8="real18";
+			String ap9="real10";
+			String AppVersion[]= {ap,ap1,ap2,ap3,ap4,ap5,ap6,ap7,ap8,ap9};
+			for (String x:AppVersion) {
+				System.out.println("PanCard no:"+x);
+			}
+		 //specs model no
+			String sp="ral1";
+			String sp1="eal17";
+			String sp2="rel12";
+			String sp3="rea11";
+			String sp4="real14";
+			String sp5="rea12";
+			String sp6="rea13";
+			String sp7="rel15";
+			String sp8="rel18";
+			String sp9="rel10";
+			String specsModel[]= {ap,ap1,ap2,ap3,ap4,ap5,ap6,ap7,ap8,ap9};
+			for (String x:specsModel) {
+				System.out.println("specsModel no:"+x);
+			}
+		//IfSC cord
+			String ic="EEDPsdf3er35";
+			String ic1="EEDPBer3ert";
+			String ic2="EEDPBheg233";
+			String ic3="EEDPB2ert33";
+			String ic4="EEDPB23ereN";
+			String ic5="EEDPB3rer5B";
+			String ic6="EED2335erwV";
+			String ic7="EPB233ert5X";
+			String ic8="EPB2335ertS";
+			String ic9="DPB233ase5Z";
+			String Ifsc[]= {ic,ic1,ic2,ic3,ic4,ic5,ic6,ic7,ic8,ic9};
+			for (String p:Ifsc) {
+				System.out.println("Ifsc no:"+p);
+			}
+			
+		//Home Add
+			String ha="home123";
+			String ha1="home234";
+			String ha2="home34";
+			String ha3="home5";
+			String ha4="home45";
+			String ha5="home23";
+			String ha6="home55";
+			String ha7="home77";
+			String ha8="home433";
+			String ha9="home23";
+			String homeAdd[]= {ha,ha1,ha2,ha3,ha4,ha5,ha6,ha7,ha8,ha9};
+			for (String p:homeAdd) {
+				System.out.println("home Address no:"+p);
+			}
+			//password
+			String ps="Pass@123";
+			String ps1="Pass@133";
+			String ps2="Pass@1253";
+			String ps3="Pass@1273";
+			String ps4="Pass@12323";
+			String ps5="Pass@12383";
+			String ps6="Pass@1236";
+			String ps7="Pass@1239";
+			String ps8="Pass@1237";
+			String ps9="Pass@1239";
+			String password[]= {ps,ps1,ps2,ps3,ps4,ps5,ps6,ps7,ps8,ps9};
+			for (String p:password) {
+				System.out.println("password no:"+p);
+			}
+			//facebook id
+			String fb="abhi_bad";
+			String fb1="abhi_badg";
+			String fb2="abhi_badj";
+			String fb3="abhi_bada";
+			String fb4="abhij_bad";
+			String fb5="abhije_bad";
+			String fb6="abh_bad";
+			String fb7="abi_bad";
+			String fb8="ahi_bad";
+			String fb9="abhi";
+			String faceBook[]= {fb,fb1,fb2,fb3,fb4,fb5,fb6,fb7,fb8,fb9};
+			for (String p:faceBook) {
+				System.out.println("faceBook no:"+p);
+			}
+	}
+
+}
